d74779a4c4158b99b3c7cb8c780324ed4616fad7
TannerParks <59822998+TannerParks@users.noreply.github.com>
2021-03-08 11:56:43 -0800

    Add files via upload

--- BetterHashTable.cpp
+++ BetterHashTable.cpp
@@ -14,23 +14,51 @@ int find_value(string str){
 
     return str_val;
 }
-HashNode *add_value(string word, HashNode *array[]){
-    HashNode *new_node;
-    //HashNode array[50];
+HashNode *add_value(string word, HashNode *array){  //nothing is being returned anymore so I can probably just use void
+    HashNode new_node;
+    //HashNode current;
     int value;  //this is the key for the word in the table
 
     value = find_value(word);   //gets the key from here
 
-    cout << word << " has a value of " << value << endl;
+    if (value > 50){    //if the key is larger than the array then make it fit by using the % sign
+        value = value % 50;
+        cout << "The value of the string is: " << value << endl;
+    }
+    new_node.key = value;   //these things add the information to the HashNode
+    new_node.word = word;
 
-    return new_node;
+    if (array[value].key != -1){    //checks to see if the key is -1 since that's its default in the HashNode.h file
+        array[value].next = &new_node;  //makes the new_node the next one after the one already there
+    }
+    else{
+        array[value] = new_node;
+    }
+
+    return array;
 }
 
-int main(){
-    HashNode *new_node;
-    HashNode *array[50];
+void display(HashNode *array, int value){
+    cout << "Word at array " << value << " is " << array[value].word << endl;
+}
 
-    new_node = add_value("ZXZ", array);
-    array[new_node->key] = new_node;
+
+int main(){
+    HashNode new_node;
+    HashNode array[50];
+    HashNode *arrayptr = array;
+
+    //add_value("ZXZ", arrayptr);
+    //array[new_node.key] = new_node;
+    //display(arrayptr, 26);
+
+    add_value("dad", arrayptr);
+    //array[new_node.key] = new_node;
+    display(arrayptr, 9);
+
+    add_value("add", arrayptr);
+    //array[new_node.key] = new_node;
+    display(arrayptr, 9);
+
 
 }
